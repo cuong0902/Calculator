3022bca9417053842f90810611e2df56ac0d07f5
manh cuong <2051052015cuong@ou.edu.vn>
2022-10-15 14:43:06 +0700

    them

--- Caculator/Form1.cs
+++ Caculator/Form1.cs
@@ -17,20 +17,6 @@ namespace Caculator
             InitializeComponent();
         }
 
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label3_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void textBox3_TextChanged(object sender, EventArgs e)
-        {
-
-        }
 
         private void button1_Click(object sender, EventArgs e)
         {

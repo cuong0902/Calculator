7300c4c3f30bbe78625c8c586d1b0ab537ac9627
cuong0906 <2051052015cuong@ou.edu.vn>
2023-11-24 11:29:25 +0700

    he so 16

--- CalculatorTest/TestBaiTap.cs
+++ CalculatorTest/TestBaiTap.cs
@@ -74,5 +74,41 @@ namespace TestBaiTap
             BaiTap2 p = new BaiTap2(n, a);
             Assert.AreEqual(expected, p.Cal(x));
         }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestRadix1()
+        {
+            int n = -1;
+            Radix r = new Radix(n);
+        }
+      
+        [TestMethod]
+        public void TestRadix2()
+        {
+
+            int r = 2;
+            int n = 6;
+            Radix radix = new Radix(n);
+            string expected = "110";
+            Assert.AreEqual(expected, radix.ConvertDecimalToAnother(r));
+        }
+
+        public TestContext TestContext { get; set; }
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.CSV",
+            @"D:\Calculator\CalculatorTest\Data\Bai03.csv", "Bai03#csv", DataAccessMethod.Sequential)]
+        [TestMethod]
+        public void TestRadix3()
+        {
+
+            int n = int.Parse(TestContext.DataRow[0].ToString());
+            int r = int.Parse(TestContext.DataRow[1].ToString()); ;
+            Radix radix = new Radix(n);
+            string expected = TestContext.DataRow[2].ToString();
+
+           // expected = expected.Remove(0, 1);
+            Assert.AreEqual(expected, radix.ConvertDecimalToAnother(r));
+        }
     }
+
 }
